85e8e79e2262eea756a5dfee5c0ba834e42fb740
Teo418 <teoibanham@gmail.com>
2026-04-18 00:21:25 -0300

    Subo las escenas de personajes con escena jugador, tambien la escenas juego

---
+++ beat-em-up/escenas/pj.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+# esta es la "clase padre" (mas bien la escena padre),
+# aca van a estar los atributos que luego los van a heredar
+# tanto los enemigos como los jugadores
+
+@export var vida : int
+@export var danio : int
+@export var velocidad : int
+
+# el export te lleva las variables a la barra derecha
