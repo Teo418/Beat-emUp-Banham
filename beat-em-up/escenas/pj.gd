1304df8c291fdf259980d9ca24eb12978a7c0273
Teo418 <teoibanham@gmail.com>
2026-04-21 11:59:55 -0300

    estoy teniendo problemas al agregar codigo al script del personaje, agregue animaciones igualmente

--- beat-em-up/escenas/pj.gd
+++ beat-em-up/escenas/pj.gd
@@ -8,32 +8,24 @@ extends CharacterBody2D
 @export var danio : int
 @export var velocidad : int
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * velocidad
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, velocidad)
 
 	move_and_slide()
 	
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("Pegar")):
-		var bodies :Array= $Area2D.get_overlapping_bodies()
+		var bodies :Array= $HitboxGolpesEnemigos.get_overlapping_bodies()
 		if(bodies.size() == 0):
 			return
 		bodies.front().queue_free()
